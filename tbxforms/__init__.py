3ef743e2bbb81632df5968249ec7ac256fa3282c
Kyle Bayliss <kyle.bayliss@torchbox.com>
2021-04-14 10:06:39 +0100

    Remove second embedded license

--- tbxforms/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-A Torchbox-flavoured template pack for `django-crispy-forms`_, allowing you to
-generate consistent, usable and accessible forms in your Django projects.
-"""
-__version__ = "0.0.0"
-__author__ = "Andy Babic"
-__email__ = "andy.babic@torchbox.com"
-__license__ = "BSD 2-Clause 'Simplified' License"
-__copyright__ = "Copyright (C) 2020 Torchbox Ltd."
