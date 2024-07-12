0684d2fd1aa5cf9b0c929801f8c633789d482dde
Kyle Bayliss <kyle.bayliss@torchbox.com>
2024-07-12 09:05:51 +0100

    Fix tests

--- tests/layout/test_errors.py
+++ tests/layout/test_errors.py
@@ -18,16 +18,19 @@ def form():
     return CheckboxForm(data={"accept": True})
 
 
-def test_no_error(self, form, snapshot_html):
+def test_no_error(form, snapshot_html):
     """
-    Test default form used for tests is valid.
+    Test default form used for tests is valid and no error title appears.
     """
 
     assert form.is_valid()
-    assert render_form(form) == snapshot_html
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" not in rendered_form
 
 
-def test_default_error_title(self, form, snapshot_html):
+def test_default_form_error_title(form, snapshot_html):
     """
     Test default error title shows if there's an error.
     """
@@ -39,7 +42,7 @@ def test_default_error_title(self, form, snapshot_html):
     assert "There is a problem with your submission" in rendered_form
 
 
-def test_custom_error_title(self, form, snapshot_html):
+def test_custom_form_error_title(form, snapshot_html):
     """
     Test custom error title shows if there's an error.
     """
@@ -52,7 +55,7 @@ def test_custom_error_title(self, form, snapshot_html):
     assert "Uh-oh!" in rendered_form
 
 
-def test_markup_allowed_in_non_field_error(self, form, snapshot_html):
+def test_markup_allowed_in_non_field_error(form, snapshot_html):
     """
     Test markup is rendered for non-field errors.
     """
@@ -64,7 +67,7 @@ def test_markup_allowed_in_non_field_error(self, form, snapshot_html):
     assert "<i>with markup</i>" in rendered_form
 
 
-def test_markup_allowed_in_field_error(self, form, snapshot_html):
+def test_markup_allowed_in_field_error(form, snapshot_html):
     """
     Test markup is rendered for non-field errors.
     """
