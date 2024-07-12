90cbb4bee41eedf161df5ca40aaefeb550052b3d
Kyle Bayliss <kbayliss@users.noreply.github.com>
2024-07-12 15:48:56 +0100

    Allow markup in field and non-field error messages (#79)

---
+++ tests/layout/test_errors.py
@@ -0,0 +1,110 @@
+"""
+Tests to verify form and formset errors show.
+"""
+
+from django.utils.html import mark_safe
+
+import pytest
+
+from tests.forms import CheckboxForm
+from tests.utils import render_form
+
+
+@pytest.fixture
+def form():
+    """
+    A valid form to use as a base for all tests.
+    """
+    return CheckboxForm(data={"accept": True})
+
+
+def test_no_error(form, snapshot_html):
+    """
+    Test default form used for tests is valid and no error title appears.
+    """
+
+    assert form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" not in rendered_form
+
+
+def test_default_form_error_title(form, snapshot_html):
+    """
+    Test default error title shows if there's an error.
+    """
+    form.add_error(None, "An example non-field error.")
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" in rendered_form
+
+
+def test_custom_form_error_title(form, snapshot_html):
+    """
+    Test custom error title shows if there's an error.
+    """
+    form.helper.form_error_title = "Uh-oh!"
+    form.add_error(None, "An example non-field error.")
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" not in rendered_form
+    assert "Uh-oh!" in rendered_form
+
+
+def test_safe_markup_in_non_field_error(form, snapshot_html):
+    """
+    Test safe markup is rendered for non-field errors.
+    """
+    form.add_error(None, mark_safe("Non-field error <i>with markup</i>."))
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "<i>with markup</i>" in rendered_form
+    assert "&lt;i&gt;with markup&lt;/i&gt;" not in rendered_form
+
+
+def test_unsafe_markup_in_non_field_error(form, snapshot_html):
+    """
+    Test unsafe markup does not get automatically marked as safe for non-field
+    errors.
+    """
+    form.add_error(None, "Non-field error <i>with markup</i>.")
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "<i>with markup</i>" not in rendered_form
+    assert "&lt;i&gt;with markup&lt;/i&gt;" in rendered_form
+
+
+def test_safe_markup_in_field_error(form, snapshot_html):
+    """
+    Test markup is rendered for field errors.
+    """
+    form.add_error("accept", mark_safe("Field error <i>with markup</i>."))
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "<i>with markup</i>" in rendered_form
+    assert "&lt;i&gt;with markup&lt;/i&gt;" not in rendered_form
+
+
+def test_unsafe_markup_in_field_error(form, snapshot_html):
+    """
+    Test unsafe markup does not get automatically marked as safe for field
+    errors.
+    """
+    form.add_error("accept", "Field error <i>with markup</i>.")
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "<i>with markup</i>" not in rendered_form
+    assert "&lt;i&gt;with markup&lt;/i&gt;" in rendered_form
