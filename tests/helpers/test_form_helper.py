e53545df3c2f5540fc511de2ecb6c323c64de221
Kyle Bayliss <kyle.bayliss@torchbox.com>
2025-02-11 16:31:48 +0000

    Fix and include tests

--- tests/helpers/test_form_helper.py
+++ tests/helpers/test_form_helper.py
@@ -9,6 +9,7 @@ from tbxforms.layout import (
 )
 from tests.forms import (
     CheckboxesForm,
+    CheckboxForm,
     TextInputForm,
 )
 from tests.utils import render_form
@@ -44,3 +45,26 @@ def test_override_default_legend_size(snapshot_html):
         Field.checkboxes("method", legend_size=Size.LARGE)
     )
     assert render_form(form) == snapshot_html
+
+
+def test_error_summary_shown_by_default(snapshot_html):
+    """Verify the error summary is shown by default."""
+    form = CheckboxForm(data={"accept": True})
+    form.add_error(None, "An example non-field error.")
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" in rendered_form
+
+
+def test_error_summary_can_be_hidden(snapshot_html):
+    """Verify the error summary can be hidden."""
+    form = CheckboxForm(data={"accept": True})
+    form.add_error(None, "An example non-field error.")
+    form.helper.show_error_summary = False
+    assert not form.is_valid()
+
+    rendered_form = render_form(form)
+    assert rendered_form == snapshot_html
+    assert "There is a problem with your submission" not in rendered_form
